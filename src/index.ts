0b2fd7ff942cf1f425f690f0e207cdf4daa9d2e4
Carlos de la Orden <cdelaorden@gmail.com>
2023-05-21 18:47:57 +0200

    chore: remove fake message

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config()
-import { buildApp } from './app'
 import env from './lib/env'
+import { buildApp } from './app'
 
 buildApp(env.int('PORT', 3000))
 
