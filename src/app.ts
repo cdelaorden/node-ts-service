021eff18e3b6d5466672f166696507320d79861e
Carlos de la Orden <cdelaorden@gmail.com>
2023-05-21 18:32:13 +0200

    feat: add express health check endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
+import express from 'express'
 
 export function buildApp(port: number){
-  console.log('Starting app at port', port)
+  const app = express()
+  app.get('/healthz', (_req, res) => {
+    res.status(200).send('all good!')
+  })
+  app.listen(port, () => {
+    console.log('Started app at port', port)
+  })
   // do your stuff, spin up your express server or whatever
   setTimeout(() => console.log('Nothing to do, edit app.ts!!!'), 1000)
 }
